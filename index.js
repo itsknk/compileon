675816e97db7a03d078da1a00c19de66d546421a
itsknk <kishorekaranam99@gmail.com>
2020-11-18 18:18:31 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var cuid = require('cuid');
+var exec = require('child_process').exec;
+var javaCompiler = require('./lib/javaC');
+
+exports.stats = false;
+
+initialize = function(option)
+{
+    if(option)
+    {
+        if(option.stats === true)
+        {
+            console.log("Stats are selected");
+            exports.stats = true;
+        }
+    }
+    fs.exists('./files', function(exists){
+        if(!exists)
+        {
+            if(exports.stats)
+            {
+                console.log("Files folder created");
+            }
+            fs.mkdirSync('./files');
+        }
+    });
+}
+
+javaCompile = function(envData, code, fun)
+{
+    if(exports.stats)
+        javaCompiler.stats = true;
+    javaCompiler.javaCompile(envData, code, fun);
+}
+
+compileWithInput = function(envData, code, inp, fun)
+{
+    if(exports.stats)
+        javaCompiler.stats = true;
+    javaCompiler.compileWithInput(envData, code, inp, fun);
+}
+
+exports.initialize = this.initialize;
+exports.javaCompile = this.javaCompile;
+exports.compileWithInput = this.compileWithInput;
